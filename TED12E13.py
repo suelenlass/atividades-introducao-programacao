0f492cf44e96caea1ec795596cd3e8fe79afe0c0
Suelen Laís <103386039+suelenlass@users.noreply.github.com>
2022-05-13 23:50:34 -0300

    TED 12 E 13

---
+++ TED12E13.py
@@ -0,0 +1,10 @@
+glossario = {
+'String' : 'Sequencia de caracteres',
+'Visual Studio Code' : 'IDE',
+'Python' : 'Linguagem de programação',
+'Git' : 'Sistema de versionamento de código',
+'Algoritmo' : 'Sequência lógica de passos finitos'
+}
+
+for i in glossario :
+   print(f'O significado de {i} é: \n - {glossario[i]}')
